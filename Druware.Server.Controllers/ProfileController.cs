0a00a6dac9df8317722c287827a91bfb9ea9eb66
Drufus <dru@openbcm.com>
2025-12-02 21:52:20 -0500

    Starting Mfa Support

---
+++ Druware.Server.Controllers/ProfileController.cs
@@ -0,0 +1,160 @@
+using System.Web;
+using Druware.Server.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using RESTfulFoundation.Server;
+
+namespace Druware.Server.Controllers;
+
+    /// <summary>
+    /// The ProfileController provides all of the functionality for the
+    /// currently logged in user to alter and adjust their user information and
+    /// options.
+    /// </summary>
+    [Route("api/[controller]")]
+    public class ProfileController : CustomController
+	{
+        public ProfileController(
+            IConfiguration configuration,
+            UserManager<User> userManager,
+            SignInManager<User> signInManager,
+            ServerContext userContext) : base(
+                configuration, userManager, signInManager, userContext)
+        {
+        }
+
+        [HttpGet("")]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var user = await UserManager.GetUserAsync(HttpContext.User);
+                return user == null ? 
+                    Ok(Result.Error("No User Logged In")) : 
+                    Ok(user);
+            }
+            catch (Exception exc)
+            {
+                return Ok(Result.Error(exc.Message));
+            }
+        }
+
+        [HttpGet("mfa")]
+        [Authorize(Roles = UserSecurityRole.Confirmed)]
+        public async Task<IActionResult> GetMfaToken()
+        {
+            try
+            {
+                var user = await UserManager.GetUserAsync(HttpContext.User);
+                if (user == null) return Ok(Result.Error("No User Logged In"));
+
+                var token = await UserManager.GetAuthenticatorKeyAsync(user);
+                
+                // otpauth://totp/{0}:{1}?secret={2}&issuer={0}&digits=6
+
+                var tokenUrl =
+                    $"otpauth://totp/{HttpUtility.UrlEncode("Trustwin")}:{HttpUtility.UrlEncode(user.Email)}?secret={token}&issuer={HttpUtility.UrlEncode("Trustwin")}&digits=6";
+                
+                
+                if (token != null) return Ok(Result.Ok(tokenUrl));
+                await UserManager.ResetAuthenticatorKeyAsync(user);
+                token = await UserManager.GetAuthenticatorKeyAsync(user);
+                return Ok(Result.Ok(tokenUrl));
+
+            }
+            catch (Exception exc)
+            {
+                return Ok(Result.Error(exc.Message));
+            }
+        }
+        
+        // TODO: Complete this implementation to enable round trip MFA 
+        [HttpPost("mfa")]
+        [Authorize(Roles = UserSecurityRole.Confirmed)]
+        public async Task<IActionResult> VerifyAuthenticator([FromBody] MfaAuthenicator verifyAuthenticator)
+        {
+            // code omitted
+            try
+            {
+                var user = await UserManager.GetUserAsync(HttpContext.User);
+                if (user == null) return Ok(Result.Error("No User Logged In"));
+                
+                var verificationCode = verifyAuthenticator.Code.Replace(" ", string.Empty).Replace("-", string.Empty);
+     
+                var is2FaTokenValid = await UserManager.VerifyTwoFactorTokenAsync(
+                    user, UserManager.Options.Tokens.AuthenticatorTokenProvider, verificationCode);
+     
+                await UserManager.SetTwoFactorEnabledAsync(user, true);
+     
+                var result = new MfaAuthenicator
+                {
+                    Status = "Success",
+                    Message = "Your authenticator app has been verified",
+                };
+     
+                if (await UserManager.CountRecoveryCodesAsync(user) != 0) return Ok(result);
+     
+                var recoveryCodes = await UserManager.GenerateNewTwoFactorRecoveryCodesAsync(user, 10);
+                result.RecoveryCodes = recoveryCodes?.ToArray() ?? [];
+                return Ok(result);
+            }
+            catch (Exception exc)
+            {
+                return Ok(Result.Error(exc.Message));
+            }
+        }
+        
+        
+        [HttpPut("")]
+        [Authorize(Roles = UserSecurityRole.Confirmed)]
+        public async Task<IActionResult> Put([FromBody] User model)
+        {
+            try
+            {
+                // get the current user
+                var user = await UserManager.GetUserAsync(HttpContext.User);
+                if (user == null) return Ok(Result.Error("No User Logged In")); 
+                
+                // compare the user to the model.
+                if (model.Id != user.Id)
+                    return BadRequest("Id mismatch");
+                
+                // update the user fields with the
+                if (user.FirstName != model.FirstName)
+                    user.FirstName = model.FirstName;
+                if (user.LastName != model.LastName)
+                    user.LastName = model.LastName;
+                if (user.Email != model.Email)
+                    user.Email = model.Email;
+                if (user.PhoneNumber != model.PhoneNumber)
+                    user.PhoneNumber = model.PhoneNumber;
+
+                return await ServerContext.SaveChangesAsync() < 1 ? 
+                    Ok(Result.Error("User Save Failed")) : Ok(user);
+            }
+            catch (Exception exc)
+            {
+                return Ok(Result.Error(exc.Message));
+            }
+        }
+
+        [HttpGet("permission/{role}")]
+        [Authorize(Roles = UserSecurityRole.Confirmed)]
+        public async Task<IActionResult> CheckPermission(string role)
+        {
+            try
+            {
+                User? user = await UserManager.GetUserAsync(HttpContext.User);
+                return (await UserManager.IsInRoleAsync(user, role)) ?
+                    Ok(Result.Ok("Granted")) : Ok(Result.Error("Denied"));
+            }
+            catch (Exception exc)
+            {
+                return BadRequest(exc.Message);
+            }
+        }
+        
+
+    }
--- Druware.Server.Controllers/UserController.cs
+++ Druware.Server.Controllers/UserController.cs
@@ -18,7 +18,6 @@
  *    All Rights Reserved
  */
 
-using System;
 using AutoMapper;
 using Druware.Server.Entities;
 using Druware.Server.Models;
@@ -29,259 +28,266 @@ using Microsoft.Extensions.Configuration;
 using System.Reflection;
 using System.Web;
 using Microsoft.AspNetCore.Authorization;
-using System.Data;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Routing;
 
-namespace Druware.Server.Controllers
+namespace Druware.Server.Controllers;
+
+[Route("api/[controller]")]
+// [Route("[controller]")]
+public class UserController : CustomController
 {
-    [Route("api/[controller]")]
-    // [Route("[controller]")]
-    public class UserController : CustomController
+    // private readonly IConfiguration _configuration;
+    private readonly IMapper _mapper;
+    private readonly RoleManager<Role> _roleManager;
+
+    private readonly AppSettings _settings;
+
+    public UserController(
+        IConfiguration configuration,
+        IMapper mapper,
+        UserManager<User> userManager,
+        RoleManager<Role> roleManager,
+        SignInManager<User> signInManager,
+        ServerContext context)
+        : base(configuration, userManager, signInManager, context)
     {
-        // private readonly IConfiguration _configuration;
-        private readonly IMapper _mapper;
-        private readonly RoleManager<Role> _roleManager;
+        _settings = new AppSettings(Configuration);
+        _mapper = mapper;
+        _roleManager = roleManager;
+    }
 
-        private readonly AppSettings _settings;
+    #region Base
+    
+    [HttpGet("")]
+    [Authorize(Roles = UserSecurityRole.ManagerOrSystemAdministrator)]
+    public async Task<ActionResult<ListResult>> GetList([FromQuery] int page = 0, [FromQuery] int count = 1000)
+    {
+        var r = await UpdateUserAccess();
+        if (r != null) return r;
 
-        public UserController(
-            IConfiguration configuration,
-            IMapper mapper,
-            UserManager<User> userManager,
-            RoleManager<Role> roleManager,
-            SignInManager<User> signInManager,
-            ServerContext context)
-            : base(configuration, userManager, signInManager, context)
+        try
         {
-            _settings = new AppSettings(Configuration);
-            _mapper = mapper;
-            _roleManager = roleManager;
+            var total = UserManager.Users.Count();
+            var list = UserManager.Users
+                .OrderBy(a => a.LastName)
+                .ThenBy(a => a.FirstName)
+                .Skip(page * count)
+                .Take(count)
+                .AsNoTracking()
+                .ToList();
+            var result = ListResult.Ok(list, total, page, count);
+            return Ok(result);
         }
-
-        [HttpGet("")]
-        [Authorize(Roles = UserSecurityRole.ManagerOrSystemAdministrator)]
-        public async Task<ActionResult<ListResult>> GetList([FromQuery] int page = 0, [FromQuery] int count = 1000)
+        catch (Exception exc)
         {
-            ActionResult? r = await UpdateUserAccess();
-            if (r != null) return r;
-
-            try
-            {
-                if (UserManager.Users == null) return BadRequest(); // think I want to alter this to not need the Ok()
-
-                var total = UserManager.Users?.Count() ?? 0;
-                var list = UserManager.Users?
-                    .OrderBy(a => a.LastName)
-                    .ThenBy(a => a.FirstName)
-                    .Skip(page * count)
-                    .Take(count)
-                    .AsNoTracking()
-                    .ToList();
-                if (list == null)
-                {
-                    return BadRequest("No List Returned");
-                }
-                ListResult result = ListResult.Ok(list, total, page, count);
-                return Ok(result);
-            }
-            catch (Exception exc)
-            {
-                return BadRequest(exc.Message);
-            }
-        }
-
-        [HttpGet("{value}")]
-        [Authorize(Roles = UserSecurityRole.ManagerOrSystemAdministrator)]
-        public async Task<ActionResult<User>> Get(string value)
-        {
-            ActionResult? r = await UpdateUserAccess();
-            if (r != null) return r;
-
-            try
-            {
-                User? user = await UserManager.FindByIdAsync(value);
-                return (user != null) ? Ok(user) : BadRequest("Not Found");
-            }
-            catch (Exception exc)
-            {
-                return BadRequest(exc.Message);
-            }
-        }
-
-        [HttpDelete("{value}")]
-        [Authorize(Roles = UserSecurityRole.ManagerOrSystemAdministrator)]
-        public async Task<ActionResult<Result>> Remove(string value)
-        {
-            ActionResult? r = await UpdateUserAccess();
-            if (r != null) return r;
-
-            try
-            {
-                User? user = await UserManager.FindByIdAsync(value);
-                if (user == null) return BadRequest("Not Found");
-
-                IdentityResult result = await UserManager.DeleteAsync(user);
-
-                return (result.Succeeded) ?
-                    Ok(Result.Ok("Success")) :
-                    Ok(Result.Error(result.Errors.ToString()));
-            }
-            catch (Exception exc)
-            {
-                return BadRequest(exc.Message);
-            }
-        }
-
-        [HttpGet("{value}/role")]
-        [Authorize(Roles = UserSecurityRole.ManagerOrSystemAdministrator)]
-        public async Task<ActionResult> GetUserRoles(string value)
-        {
-            ActionResult? r = await UpdateUserAccess();
-            if (r != null) return r;
-
-            try
-            {
-                User? user = await UserManager.FindByIdAsync(value);
-                if (user == null) return BadRequest("Not Found");
-
-                var roles = await UserManager.GetRolesAsync(user);
-
-                return (roles != null) ? Ok(roles.ToList()) : BadRequest("Not Found");
-            }
-            catch (Exception exc)
-            {
-                return BadRequest(exc.Message);
-            }
-        }
-
-        [HttpGet("role")]
-        [Authorize(Roles = UserSecurityRole.ManagerOrSystemAdministrator)]
-        public async Task<ActionResult> GetAvailableRoles([FromQuery] int page = 0, [FromQuery] int count = 1000)
-        {
-            ActionResult? r = await UpdateUserAccess();
-            if (r != null) return r;
-
-            try
-            {
-                var total = _roleManager.Roles?.Count() ?? 0;
-                var list = _roleManager.Roles?
-                    .OrderBy(a => a.Name)
-                    .Skip(page * count)
-                    .Take(count)
-                    .AsNoTracking()
-                    .ToList();
-                ListResult result = ListResult.Ok(list!, total, page, count);
-
-                return Ok(result);
-            }
-            catch (Exception exc)
-            {
-                return BadRequest(exc.Message);
-            }
-        }
-
-        [HttpPut("{value}/role")]
-        [Authorize(Roles = UserSecurityRole.ManagerOrSystemAdministrator)]
-        public async Task<ActionResult> UpdateUserRole(
-            string value, [FromBody] List<string> roles)
-        {
-            try
-            {
-                User? user = await UserManager.FindByIdAsync(value);
-                if (user == null) return BadRequest("Not Found");
-
-                var allRoles = _roleManager.Roles.ToList(); // adding the ToList() prevents keeping the connection open
-                var userRoles = await UserManager.GetRolesAsync(user);
-
-                // loop the allRoles to ensure that we add/remove properly
-                foreach (Role role in allRoles)
-                {
-                    // if the role exists in the userRoles, but not the posted roles
-                    // then remove it.
-                    if (!roles.Contains(role.Name) && userRoles.Contains(role.Name))
-                        await UserManager.RemoveFromRoleAsync(user, role.Name);
-
-                    // if the role exists in the posted roles but not the userRoles
-                    // the add it.
-                    if (roles.Contains(role.Name) && !userRoles.Contains(role.Name))
-                        await UserManager.AddToRoleAsync(user, role.Name);
-                }
-
-                userRoles = await UserManager.GetRolesAsync(user);
-
-                return (userRoles != null) ? Ok(userRoles.ToList()) : BadRequest("Not Found");
-            }
-            catch (Exception exc)
-            {
-                return BadRequest(exc.Message);
-            }
-        }
-        
-        [HttpPost("")]
-        [Authorize(Roles = UserSecurityRole.ManagerOrSystemAdministrator)]
-        public async Task<ActionResult<Result>> Add(
-            [FromBody] UserRegistrationModel model)
-        {
-            try
-            {
-                if (!ModelState.IsValid)
-                    return Ok(Result.Error("Invalid Model Recieved"));
-
-                var user = _mapper.Map<User>(model);
-
-                var result = await UserManager.CreateAsync(user, model.Password);
-                if (!result.Succeeded)
-                {
-                    Result errorResult = Result.Error("Unable to create user.");
-                    foreach (var error in result.Errors)
-                        errorResult.Info?.Add(error.Description);
-                    return Ok(errorResult);
-                }
-
-                var token = await UserManager.GenerateEmailConfirmationTokenAsync(user);
-
-                // build out the email message to the registered email with the
-                // confirmation link that provides the path to confirm the email.
-
-                if (_settings.Smtp == null) throw new Exception("Mail Services Not  Configured");
-
-                var link = string.Format("{0}?email={2}&token={1}",
-                    _settings.ConfirmationUrl,
-                    HttpUtility.UrlEncode(token),
-                    user.Email);
-
-                // TODO: Flesh this out to provide a nice email for confirmation,
-                //       preferably with multiple output formats ( templaste loaded from resources perhaps )
-                if (Assembly.GetEntryAssembly()?.GetName()?.Name == null)
-                    return Ok(Result.Error("Assembly Not Found"));
-                if (_settings.Notification == null)
-                    return Ok(Result.Error("Settings Not Found"));
-                Console.WriteLine(Assembly.GetEntryAssembly()!.GetName()!.Name!);
-
-                MailHelper helper = new MailHelper(_settings.Smtp, Assembly.GetEntryAssembly()!.GetName()!.Name!);
-                helper.Send(
-                    user.Email,
-                    _settings.Notification.From!,
-                    _settings.Notification.From!,
-                    "Confirmation email link",
-                    link!
-                );
-
-                await UserManager.AddToRoleAsync(user, UserSecurityRole.Unconfirmed.ToUpper());
-
-#if DEBUG
-                return Ok(Result.Ok(string.Format("{0}", HttpUtility.UrlEncode(token))));
-#else
-            return Ok(Result.Ok("User Created, limited to Visitor Status Until Confirmed."));
-#endif
-            }
-            catch (Exception exc)
-            {
-                return BadRequest(exc.Message);
-            }
+            return BadRequest(exc.Message);
         }
     }
 
+    [HttpGet("{value}")]
+    [Authorize(Roles = UserSecurityRole.ManagerOrSystemAdministrator)]
+    public async Task<ActionResult<User>> Get(string value)
+    {
+        var r = await UpdateUserAccess();
+        if (r != null) return r;
+
+        try
+        {
+            var user = await UserManager.FindByIdAsync(value);
+            return (user != null) ? Ok(user) : BadRequest("Not Found");
+        }
+        catch (Exception exc)
+        {
+            return BadRequest(exc.Message);
+        }
+    }
+
+    [HttpDelete("{value}")]
+    [Authorize(Roles = UserSecurityRole.ManagerOrSystemAdministrator)]
+    public async Task<ActionResult<Result>> Remove(string value)
+    {
+        ActionResult? r = await UpdateUserAccess();
+        if (r != null) return r;
+
+        try
+        {
+            User? user = await UserManager.FindByIdAsync(value);
+            if (user == null) return BadRequest("Not Found");
+
+            IdentityResult result = await UserManager.DeleteAsync(user);
+
+            return (result.Succeeded) ?
+                Ok(Result.Ok("Success")) :
+                Ok(Result.Error(result.Errors.ToString()));
+        }
+        catch (Exception exc)
+        {
+            return BadRequest(exc.Message);
+        }
+    }
+    
+        [HttpPost("")]
+    [Authorize(Roles = UserSecurityRole.ManagerOrSystemAdministrator)]
+    public async Task<ActionResult<Result>> Add(
+        [FromBody] UserRegistrationModel model)
+    {
+        try
+        {
+            if (!ModelState.IsValid)
+                return Ok(Result.Error("Invalid Model Recieved"));
+
+            var user = _mapper.Map<User>(model);
+
+            var result = await UserManager.CreateAsync(user, model.Password);
+            if (!result.Succeeded)
+            {
+                Result errorResult = Result.Error("Unable to create user.");
+                foreach (var error in result.Errors)
+                    errorResult.Info?.Add(error.Description);
+                return Ok(errorResult);
+            }
+
+            var token = await UserManager.GenerateEmailConfirmationTokenAsync(user);
+
+            // build out the email message to the registered email with the
+            // confirmation link that provides the path to confirm the email.
+
+            if (_settings.Smtp == null) throw new Exception("Mail Services Not  Configured");
+
+            var link = string.Format("{0}?email={2}&token={1}",
+                _settings.ConfirmationUrl,
+                HttpUtility.UrlEncode(token),
+                user.Email);
+
+            // TODO: Flesh this out to provide a nice email for confirmation,
+            //       preferably with multiple output formats ( templaste loaded from resources perhaps )
+            if (Assembly.GetEntryAssembly()?.GetName()?.Name == null)
+                return Ok(Result.Error("Assembly Not Found"));
+            if (_settings.Notification == null)
+                return Ok(Result.Error("Settings Not Found"));
+            Console.WriteLine(Assembly.GetEntryAssembly()!.GetName()!.Name!);
+
+            MailHelper helper = new MailHelper(_settings.Smtp, Assembly.GetEntryAssembly()!.GetName()!.Name!);
+            helper.Send(
+                user.Email,
+                _settings.Notification.From!,
+                _settings.Notification.From!,
+                "Confirmation email link",
+                link!
+            );
+
+            await UserManager.AddToRoleAsync(user, UserSecurityRole.Unconfirmed.ToUpper());
+
+#if DEBUG
+            return Ok(Result.Ok(string.Format("{0}", HttpUtility.UrlEncode(token))));
+#else
+            return Ok(Result.Ok("User Created, limited to Visitor Status Until Confirmed."));
+#endif
+        }
+        catch (Exception exc)
+        {
+            return BadRequest(exc.Message);
+        }
+    }
+    
+    #endregion 
+
+    #region Roles
+    
+    [HttpGet("{value}/role")]
+    [Authorize(Roles = UserSecurityRole.ManagerOrSystemAdministrator)]
+    public async Task<ActionResult> GetUserRoles(string value)
+    {
+        var r = await UpdateUserAccess();
+        if (r != null) return r;
+
+        try
+        {
+            var user = await UserManager.FindByIdAsync(value);
+            if (user == null) return BadRequest("Not Found");
+            var roles = await UserManager.GetRolesAsync(user);
+
+            return Ok(roles.ToList());
+        }
+        catch (Exception exc)
+        {
+            return BadRequest(exc.Message);
+        }
+    }
+
+    [HttpGet("role")]
+    [Authorize(Roles = UserSecurityRole.ManagerOrSystemAdministrator)]
+    public async Task<ActionResult> GetAvailableRoles([FromQuery] int page = 0, [FromQuery] int count = 1000)
+    {
+        var r = await UpdateUserAccess();
+        if (r != null) return r;
+
+        try
+        {
+            var total = _roleManager.Roles.Count();
+            var list = _roleManager.Roles
+                .OrderBy(a => a.Name)
+                .Skip(page * count)
+                .Take(count)
+                .AsNoTracking()
+                .ToList();
+            var result = ListResult.Ok(list!, total, page, count);
+
+            return Ok(result);
+        }
+        catch (Exception exc)
+        {
+            return BadRequest(exc.Message);
+        }
+    }
+
+    [HttpPut("{value}/role")]
+    [Authorize(Roles = UserSecurityRole.ManagerOrSystemAdministrator)]
+    public async Task<ActionResult> UpdateUserRole(
+        string value, [FromBody] List<string> roles)
+    {
+        try
+        {
+            var user = await UserManager.FindByIdAsync(value);
+            if (user == null) return BadRequest("Not Found");
+
+            var allRoles = _roleManager.Roles.ToList(); // adding the ToList() prevents keeping the connection open
+            var userRoles = await UserManager.GetRolesAsync(user);
+
+            // loop the allRoles to ensure that we add/remove properly
+            foreach (var role in allRoles)
+            {
+                // if the role exists in the userRoles, but not the posted roles
+                // then remove it.
+                if (role.Name != null && !roles.Contains(role.Name) && userRoles.Contains(role.Name))
+                    await UserManager.RemoveFromRoleAsync(user, role.Name);
+
+                // if the role exists in the posted roles but not the userRoles
+                // the add it.
+                if (roles.Contains(role.Name) && !userRoles.Contains(role.Name))
+                    await UserManager.AddToRoleAsync(user, role.Name);
+            }
+
+            userRoles = await UserManager.GetRolesAsync(user);
+
+            return (userRoles != null) ? Ok(userRoles.ToList()) : BadRequest("Not Found");
+        }
+        catch (Exception exc)
+        {
+            return BadRequest(exc.Message);
+        }
+    }
+    
+    #endregion
+    
+    #region Mfa
+    
+    
+    
+    #endregion
+    
+
 }
+
+
 
